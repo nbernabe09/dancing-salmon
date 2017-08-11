ba905096275a82afdefc2399cd794db82276e236
Nathan Bernabe <nbernabe09@aol.com>
2017-08-11 12:30:29 -0700

    Combined app.js and java.js, relocated BitAPI.js to the javascript directory

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,4 +1,6 @@
 // Global Variables
+  var leftNavIsOpen = false;
+  var rightNavIsOpen = false;
   var playing = false;
   var debouncedAjax = _.debounce(doAjax, 400);
   var resultsHidden = true;
@@ -6,6 +8,46 @@
   var discogsCall = new DiscogsAPIUtil();
 
 // Functions
+  function openLeftNav() {
+    $("#leftNav").css("width", "20%");
+    // $("#leftNav").css("min-width", "193px");
+    $(".main").css("marginLeft", "20%");
+    $("#leftTab").css("left", "20%");
+    $("#navbar").css("marginLeft", "20%");
+    leftNavIsOpen=!leftNavIsOpen;
+  }
+
+  function closeLeftNav() {
+    $("#leftNav").css("width", "0");
+    // $("#leftNav").css("min-width", "0");
+    $(".main").css("marginLeft", "0");
+    $("#leftTab").css("left", "0");
+    $("#navbar").css("marginLeft", "0");
+    leftNavIsOpen=!leftNavIsOpen;
+  }
+
+  function openRightNav() {
+    $("#rightNav").css("width", "20%");
+    $("#rightTab").css("right", "20%");
+    $("#bottomNav").css("width", "80%");
+    rightNavIsOpen=!rightNavIsOpen;
+  }
+
+  function closeRightNav() {
+    $("#rightNav").css("width", "0");
+    $("#rightTab").css("right", "0");
+    $("#bottomNav").css("width", "100%");
+    rightNavIsOpen=!rightNavIsOpen;
+  }
+
+  function openArtNav() {
+    $("#artistPage").css("width", "100%");
+  }
+
+  function closeArtNav() {
+    $("#artistPage").css("width", "0");
+  }
+
   function showResults() {
     $("#search-results").css("visibility", "visible");
     resultsHidden = false;
@@ -73,6 +115,26 @@
   }
 
 // Buttons
+  $("#rightTab").on("click", function() {
+    if (rightNavIsOpen) {
+      closeRightNav();
+    } else {
+      openRightNav();
+    }
+  });
+
+  $("#leftTab").on("click", function() {
+    if (leftNavIsOpen) {
+      closeLeftNav();
+    } else {
+      openLeftNav();
+    }
+  });
+
+  $(".artist").on("click", openArtNav);
+
+  $("#closeArtist").on("click", closeArtNav);
+  
   $("#musicBack").on("click", function() {
     alert("This button goes back one song (if possible)")
   });
